c6ef94f46f24c7582eb618101b4002647571df2c
Nayane da Silva <nayanecosilva@gmail.com>
2024-11-14 10:30:19 -0300

    Add files via upload

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //defina um campo privado para a dependência
+
+    private Coach myCoach;
+
+    //define um construtor para a dependência
+
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+
+
+
+
+}
