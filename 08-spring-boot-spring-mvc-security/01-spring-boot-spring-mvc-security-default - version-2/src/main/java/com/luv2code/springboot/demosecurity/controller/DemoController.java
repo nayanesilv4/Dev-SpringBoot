58d0a092f0876b0587eea36b2c45b7ecb4d0967f
Nayane da Silva <nayanecosilva@gmail.com>
2024-12-11 20:01:09 -0300

    Add files via upload

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default - version-2/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -0,0 +1,28 @@
+package com.luv2code.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String showHome(){
+
+        return "home";
+    }
+
+    // adicionar um request mapping para /leaders
+    @GetMapping("/leaders")
+    public String showLeaders(){
+
+        return "leaders";
+    }
+
+    // adicionar um request mapping para /systems
+    @GetMapping("/systems")
+    public String showSystems(){
+
+        return "systems";
+    }
+}
