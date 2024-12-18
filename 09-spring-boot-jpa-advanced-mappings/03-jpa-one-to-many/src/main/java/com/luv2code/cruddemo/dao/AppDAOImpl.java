002ddcd3b6c26ac03b5ea2e15139a5a41487f5c0
Nayane da Silva <nayanecosilva@gmail.com>
2024-12-18 20:28:51 -0300

    Add files via upload

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,75 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Course;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+
+        entityManager.persist(theInstructor);
+
+
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+
+        entityManager.remove(tempInstructor);
+
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return entityManager.find(InstructorDetail.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, theId);
+
+        tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
+        entityManager.remove(tempInstructorDetail);
+    }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(int theId) {
+
+        TypedQuery<Course> query = entityManager.createQuery(
+                "from Course where instructor.id = :data", Course.class);
+
+        query.setParameter("data", theId);
+
+        List<Course> courses = query.getResultList();
+
+        return courses;
+    }
+}
