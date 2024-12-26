1002140a3a017bd8c500f3e70be78f9feb318515
Nayane da Silva <nayanecosilva@gmail.com>
2024-12-26 20:50:58 -0300

    Add files via upload

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,21 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+    //this is where we add all of our related advice
+
+    //let´s start with an @Before advice
+
+    @Before("execution(* add*(..))")
+    public void beforeAddAccountAdvice(){
+
+        System.out.println("\n=====>>> Executing @Before advice on method");
+
+    }
+}
